6f714f7d1e5d2afdb8ee47ea74a3b95d8b757c2a
Boris Povolotsky <boristofu@gmail.com>
2019-10-28 15:31:30 +0200

    ...

---
+++ src/components/users/config.js
@@ -0,0 +1,14 @@
+const sender = {
+  auth: {
+    user: "boristofu@gmail.com",
+    pass: "b0tyl9ee"
+  },
+  from: 'Boris Povolotsky'
+}
+
+const to = sender.auth.user
+
+module.exports = {
+  sender,
+  to
+}
