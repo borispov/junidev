7d99394b3eea097af951e54c41815003d964ccd5
Boris Povolotsky <boristofu@gmail.com>
2019-11-14 15:18:04 +0200

    fixed filterTitles regexp

--- src/components/scrapers/utils/filterTitles.js
+++ src/components/scrapers/utils/filterTitles.js
@@ -1,3 +1,3 @@
 const defaultReg = new RegExp(/\bsenior|lead|experienced\b/i);
 
-module.exports = (title, reg) => reg ? !reg.test(title) : !defaultReg.test(title)
+module.exports = (title) => !defaultReg.test(title)
